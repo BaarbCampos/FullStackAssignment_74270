bc4ec3e2ac29b30e78b3eb58e9a31a223fca0534
Barbara Campos <bjcg2022@gmail.com>
2026-03-20 23:19:24 +0000

    74270 - implement InventoryService consumer for order-submitted events

---
+++ SportsStore.InventoryService/Worker.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+public class Worker : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest",
+            Port = 5672
+        };
+
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: "order-submitted",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine("📦 Order received:");
+            Console.WriteLine(message);
+        };
+
+        channel.BasicConsume(
+            queue: "order-submitted",
+            autoAck: true,
+            consumer: consumer
+        );
+
+        return Task.CompletedTask;
+    }
+}
