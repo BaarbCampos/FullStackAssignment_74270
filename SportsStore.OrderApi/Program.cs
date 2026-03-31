9b282e8ac27c042a99a48f11725e210bf1d1cd50
Barbara Campos <bjcg2022@gmail.com>
2026-03-31 19:20:12 +0100

    restore missing services and configuration in OrderApi Program.cs

--- SportsStore.OrderApi/Program.cs
+++ SportsStore.OrderApi/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Serilog;
+using SportsStore.OrderApi.Configuration;
+using SportsStore.OrderApi.Consumers;
 using SportsStore.OrderApi.Data;
 using SportsStore.OrderApi.Messaging;
 using SportsStore.OrderApi.Services;
@@ -18,6 +20,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// RabbitMQ settings
+builder.Services.Configure<RabbitMqSettings>(
+    builder.Configuration.GetSection("RabbitMq"));
+
 // Database
 builder.Services.AddDbContext<OrderDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -27,7 +33,29 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 // Application services
 builder.Services.AddScoped<IOrderService, EfOrderService>();
-builder.Services.AddScoped<IMessagePublisher, RabbitMqMessagePublisher>();
+builder.Services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
+
+// Consumers
+builder.Services.AddHostedService<InventoryConfirmedConsumer>();
+builder.Services.AddHostedService<InventoryFailedConsumer>();
+builder.Services.AddHostedService<PaymentApprovedConsumer>();
+builder.Services.AddHostedService<PaymentRejectedConsumer>();
+builder.Services.AddHostedService<ShippingCreatedConsumer>();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:5173",   // React Admin Dashboard
+                "https://localhost:7134",  // Blazor CustomerPortal
+                "http://localhost:5125"    // optional HTTP frontend
+              )
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -40,7 +68,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("FrontendPolicy");
 
 app.MapControllers();
 
